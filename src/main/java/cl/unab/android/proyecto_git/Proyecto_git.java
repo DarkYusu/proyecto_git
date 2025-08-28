eb4c4eccefea81d58ca3f075a349a5941e8b0afd
seba <sebastian@jmconstructoraspa.cl>
2025-08-28 19:59:42 -0400

    Init

---
+++ src/main/java/cl/unab/android/proyecto_git/Proyecto_git.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package cl.unab.android.proyecto_git;
+
+/**
+ *
+ * @author seba
+ */
+public class Proyecto_git {
+
+    public static void main(String[] args) {
+        
+        // Crear un menu
+        // Crear opcion para crear usuario
+        // Crear opcion para eliminar usuario
+        // Crear opcion para salir
+        
+        
+    }
+}
---
+++ src/main/java/cl/unab/android/proyecto_git/usuario/Usuario.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package cl.unab.android.proyecto_git.usuario;
+
+/**
+ *
+ * @author seba
+ */
+public class Usuario {
+    private String nombre;
+    private String mail;
+    
+}
